3a954c8c95f77bcc85eec8513345705f301f3f84
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-11-30 01:25:58 -0300

    feat:Add angularMaterial

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class MaterialModule { }
