432757ac649e408424c8dce5e105325def5225e3
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-12-01 00:11:07 -0300

    other:Add material imports

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [],
   exports: [
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatTooltipModule
   ]
 })
 export class MaterialModule { }
