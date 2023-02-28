617eeb6a5c9b0e54dd8823355ba7fa8d05c08932
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-02-27 22:00:58 -0300

    feat: add home page

---
+++ src/app/core/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
+})
+export class HomeComponent {
+  public title = "Hola, Soy Mauro!";
+  public description = "Soy un Desarrollador WEB/MOBILE Fullstack y Devops de Buenos Aires, Argentina.";
+}
