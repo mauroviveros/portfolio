1dc48d541d8552100f2259227bdea85a3b3bce2d
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-02-27 21:59:42 -0300

    feat: add social links

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+interface MenuItem{
+  title: string;
+  icon: string;
+  link: string;
+}
 
 @Component({
-  selector: 'core-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "core-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent {
+  public profileURL = "https://avatars.githubusercontent.com/u/44076914";
 
+  public socials: MenuItem[] = [
+    { title: "Instagram", icon: "instagram", link: "https://www.instagram.com/maurogontier" },
+    { title: "GitHub", icon: "github", link: "https://github.com/maurodviveros" },
+    { title: "LinkedIn", icon: "linkedin", link: "https://www.linkedin.com/in/maurod-viveros" }
+  ];
 }
