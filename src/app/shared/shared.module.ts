d97fabdcaf531a4ceba1c897772facdb66c66a81
Mauro Daniel Viveros <mviveros@teknosgroup.com>
2023-03-02 11:25:48 -0300

    feat: import angularMaterial components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
 
+import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
+import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
 
 
@@ -10,8 +13,11 @@ import { loadSvgResources } from "src/assets/icons";
 
 @NgModule({
   exports: [
+    MatBottomSheetModule,
+    MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatToolbarModule,
     MatTooltipModule
   ]
 })
