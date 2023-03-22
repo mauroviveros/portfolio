548bfb1bb2674f5851f7764a931564d83bebb1b3
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-03-21 21:54:23 -0300

    feat: :truck: move icons socials svg

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,11 +5,10 @@ import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
 
-
-import { loadSvgResources } from "src/assets/icons";
 import { PageTitleComponent } from "./components/page-title/page-title.component";
 
 @NgModule({
@@ -18,9 +17,9 @@ import { PageTitleComponent } from "./components/page-title/page-title.component
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    MatProgressBarModule,
     MatToolbarModule,
     MatTooltipModule,
-
     PageTitleComponent
   ],
   declarations: [
@@ -29,6 +28,8 @@ import { PageTitleComponent } from "./components/page-title/page-title.component
 })
 export class SharedModule { 
   constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer){
-    loadSvgResources(matIconRegistry, domSanitizer);
+    matIconRegistry.addSvgIcon("github", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/github.svg"));
+    matIconRegistry.addSvgIcon("instagram", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/instagram.svg"));
+    matIconRegistry.addSvgIcon("linkedin", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/linkedin.svg"));
   }
 }
--- src/assets/icons/index.ts
+++
@@ -1,8 +0,0 @@
-import { DomSanitizer } from "@angular/platform-browser";
-import { MatIconRegistry } from "@angular/material/icon";
-
-export const loadSvgResources = (ir: MatIconRegistry, ds: DomSanitizer) => {
-    ir.addSvgIcon("github", ds.bypassSecurityTrustResourceUrl("assets/icons/github.svg"));
-    ir.addSvgIcon("instagram", ds.bypassSecurityTrustResourceUrl("assets/icons/instagram.svg"));
-    ir.addSvgIcon("linkedin", ds.bypassSecurityTrustResourceUrl("assets/icons/linkedin.svg"));
-};
