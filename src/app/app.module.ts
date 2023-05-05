4ca1641beaccc4b9cdcaea1e84b70dd8668aaf53
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-05 04:05:19 -0300

    feat: :globe_with_meridians: add TranslateModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { TranslateModule, TranslateLoader, TranslateService } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { AppRoutingModule } from "./app.routing";
 
-import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
 import { SharedModule } from "./shared/shared.module";
+
+import { AppComponent } from "./app.component";
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     AppRoutingModule,
     SharedModule,
     CoreModule
@@ -19,4 +35,14 @@ import { SharedModule } from "./shared/shared.module";
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private translate: TranslateService
+  ){
+    const langs = ["en", "es"];
+    const browserLang = this.translate.getBrowserLang() || "en";
+    this.translate.addLangs(langs);
+
+    translate.use(langs.includes(browserLang) ? browserLang : "en");
+  }
+}
