42ac377f4cf0164ddae841937050b5af144ee97b
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-05 03:13:44 -0300

    refactor(core): :recycle: set core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app.routing";
 
 import { AppComponent } from "./app.component";
+import { CoreModule } from "./core/core.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +13,8 @@ import { AppComponent } from "./app.component";
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent],
