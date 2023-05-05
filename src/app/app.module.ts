ddb6891fc3832c8f0ac159beba935f2c671e91a3
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-05 03:08:21 -0300

    feat(core.shared): :recycle: set shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,14 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app.routing";
 
-import { SharedModule } from "./shared/shared.module";
-
 import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./core/components/navbar/navbar.component";
-import { NotFoundComponent } from "./core/components/not-found/not-found.component";
-import { HomeComponent } from "./core/pages/home/home.component";
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    NotFoundComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,35 +1,16 @@
 import { NgModule } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
+import { CommonModule } from "@angular/common";
+import { MaterialModule } from "./modules/material.module";
 
-import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
-import { MatListModule } from "@angular/material/list";
-import { MatProgressBarModule } from "@angular/material/progress-bar";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatTooltipModule } from "@angular/material/tooltip";
 
-import { PageTitleComponent } from "./components/page-title/page-title.component";
 
 @NgModule({
-  exports: [
-    MatBottomSheetModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    MatProgressBarModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    PageTitleComponent
+  declarations: [],
+  imports: [
+    CommonModule
   ],
-  declarations: [
-    PageTitleComponent
+  exports: [
+    MaterialModule
   ]
 })
-export class SharedModule { 
-  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer){
-    matIconRegistry.addSvgIcon("github", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/github.svg"));
-    matIconRegistry.addSvgIcon("instagram", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/instagram.svg"));
-    matIconRegistry.addSvgIcon("linkedin", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/linkedin.svg"));
-  }
-}
+export class SharedModule { }
