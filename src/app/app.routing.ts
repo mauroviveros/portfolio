df9682fd413983dea629674aa75232484ca83897
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-05 03:12:14 -0300

    refactor: :recycle: set app module/component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,26 +1,10 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-import { NotFoundComponent } from "./core/components/not-found/not-found.component";
-import { HomeComponent } from "./core/pages/home/home.component";
-
-const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent
-  },
-  {
-    path: "skills",
-    loadChildren: () => import("./modules/skills/skills.module").then(m => m.SkillsModule)
-  },
-  {
-    path: "**",
-    component: NotFoundComponent
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
