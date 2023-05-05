00882454e35d5d7e33fc11392549d2bc60ba74f5
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-05 04:59:27 -0300

    fix: :building_construction: update user data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {};
 export const USER = {
   name: ["MAURO", "DANIEL"],
   surname: ["VIVEROS"],
-  role: "DEV WEB | DEV-OPS",
+  role: "DEV-WEB | DEV-OPS",
   profileURL: "https://avatars.githubusercontent.com/u/44076914",
   country: "ARGENTINA",
   city: "BUENOS AIRES"
