4fca801ca91f4a48accc00dcdc3e00d94ef806fc
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-05 04:31:13 -0300

    feat: :sparkles: add environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {};
+
+export const USER = {
+  name: ["MAURO", "DANIEL"],
+  surname: ["VIVEROS"],
+  role: "DEV WEB | DEV-OPS",
+  profileURL: "https://avatars.githubusercontent.com/u/44076914",
+  country: "ARGENTINA",
+  city: "BUENOS AIRES"
+}
