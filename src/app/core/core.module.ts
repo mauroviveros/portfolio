69ce3b6e3eb33817618c82b2598d325e266f65bc
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-05 15:15:36 -0300

    feat(core): :wheelchair: add routing module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { CoreRoutingModule } from "./core.routing";
+import { TranslateModule } from "@ngx-translate/core";
 import { SharedModule } from "../shared/shared.module";
+
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
+    CoreRoutingModule,
+    TranslateModule.forChild(),
     SharedModule
   ],
   exports: [
