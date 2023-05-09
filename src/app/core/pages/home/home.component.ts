a1b32c2e9d9aa3d7271cc08016fef63ce1a9bb18
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-08 21:50:57 -0300

    feat(core): :globe_with_meridians: add fabMenu for updateLang

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { FabMenuItem } from "src/app/shared/interfaces/fab-menu";
+
+
 import { USER } from "src/environments/environment";
 
 @Component({
@@ -6,6 +10,30 @@ import { USER } from "src/environments/environment";
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private translate = inject(TranslateService);
   user = USER;
+  langMenu: FabMenuItem[] = [
+    {
+      _id: "es",
+      imgURL: "https://hatscripts.github.io/circle-flags/flags/es.svg",
+    },
+    {
+      _id: "en",
+      imgURL: "https://hatscripts.github.io/circle-flags/flags/uk.svg"
+    }
+  ];
+
+  ngOnInit(){
+    this._activeBrowserLang();
+  }
+
+  private _activeBrowserLang(){
+    const browserLang = this.translate.getBrowserLang();
+    this.langMenu.map(langItem => langItem.active = langItem._id === browserLang);
+  }
+
+  updateLang(lang: string){
+    this.translate.use(lang);
+  }
 }
