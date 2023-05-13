a4c68db15713b2aa6543287794e19c97a7f35af0
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-13 00:41:05 -0300

    feat: :technologist: rename root selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, HostBinding, inject } from "@angular/core";
+import { ThemeService } from "./shared/services/theme.service";
 
 @Component({
-  selector: "app-root",
+  selector: "portfolio-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent {
+  private themeService = inject(ThemeService);
+
+  @HostBinding("class") get className() { return this.themeService.theme; }
+}
