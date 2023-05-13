891c59ea68044f9969d2049a763dc87d5ea41277
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-13 00:41:31 -0300

    feat(core): :sparkles: add event onSelect theme

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, inject } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { FabMenuItem } from "src/app/shared/interfaces/fab-menu";
+import { ThemeService } from "src/app/shared/services/theme.service";
 
 
 import { USER } from "src/environments/environment";
@@ -11,18 +12,20 @@ import { USER } from "src/environments/environment";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  private theme = inject(ThemeService);
   private translate = inject(TranslateService);
   user = USER;
+
   langMenu: FabMenuItem[] = [
-    {
-      _id: "es",
-      imgURL: "https://hatscripts.github.io/circle-flags/flags/es.svg",
-    },
-    {
-      _id: "en",
-      imgURL: "https://hatscripts.github.io/circle-flags/flags/uk.svg"
-    }
+    { _id: "es", imgURL: "https://hatscripts.github.io/circle-flags/flags/es.svg" },
+    { _id: "en", imgURL: "https://hatscripts.github.io/circle-flags/flags/uk.svg" }
   ];
+
+  themeMenu: FabMenuItem[] = [
+    { _id: "theme-primary", icon: "colorize", class: "theme-primary" },
+    { _id: "theme-secondary", icon: "colorize", class: "theme-secondary" },
+    { _id: "theme-tertiary", icon: "colorize", class: "theme-tertiary" }
+  ]
 
   ngOnInit(){
     this._activeBrowserLang();
@@ -42,5 +45,9 @@ export class HomeComponent implements OnInit {
 
   updateLang(lang: string){
     this.translate.use(lang);
+  }
+
+  updateTheme(className: string){
+    this.theme.update(className);
   }
 }
