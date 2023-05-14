6952cd144165f7cc6aa1f0af45795bfb3bff54dc
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-14 20:12:49 -0300

    feat(core): :wheelchair: add about routing

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,13 +6,15 @@ import { SharedModule } from "../shared/shared.module";
 
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
