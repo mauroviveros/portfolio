bc88d08f07a447f2f679b27dec3664516cfcda2f
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-14 21:33:07 -0300

    feat: :card_file_box: update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,15 @@ export const USER = {
   role: "DEV-WEB | DEV-OPS",
   profileURL: "https://avatars.githubusercontent.com/u/44076914",
   country: "ARGENTINA",
-  city: "BUENOS AIRES"
+  city: "BUENOS AIRES",
+  copyright: {
+    year: 2022,
+    title: "Mauro Daniel Viveros",
+    site: "https://github.com/maurodviveros",
+  },
+  social: [
+    { icon: "instagram", url: "https://instagram.com/maurogontier", tooltip: "Instagram", svg: true },
+    { icon: "linkedin", url: "https://www.linkedin.com/in/maurod-viveros/", tooltip: "LinkedIn", svg: true },
+    { icon: "github", url: "https://github.com/maurodviveros", tooltip: "GitHub", svg: true },
+  ]
 }
