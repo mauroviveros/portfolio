0efec0db529449a522af06a7d92d3f6325667acd
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-15 12:44:45 -0300

    feat(core): :adhesive_bandage: get all markdowns and change by lang

--- src/app/core/pages/about/about.component.ts
+++ src/app/core/pages/about/about.component.ts
@@ -1,21 +1,40 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, OnInit, inject } from "@angular/core";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
+import { Subscription } from "rxjs";
+
+interface Markdowns{
+  [key: string]: string
+}
 
 @Component({
   selector: "core-about",
   templateUrl: "./about.component.html",
   styleUrls: ["./about.component.scss"]
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy {
+  private subscription?: Subscription;
   private http = inject(HttpClient);
   private translate = inject(TranslateService);
-  markdown?: string;
+  lang = this.translate.currentLang === "es" ? "es" : "en";
+  markdowns: Markdowns = {};
 
   ngOnInit(){
-    const lang = this.translate.currentLang === "es" ? "es" : "en";
-    this.http.get(`assets/markdown/about-${lang}.md`, { responseType: "text" }).subscribe(data => {
-      this.markdown = data;
+    this.getLangs();
+
+    this.subscription = this.translate.onLangChange.asObservable().subscribe(({ lang }) => {
+      this.lang = lang === "es" ? "es" : "en";
     })
+  }
+  ngOnDestroy(){
+    this.subscription?.unsubscribe();
+  }
+  
+  private getLangs(){
+    this.translate.getLangs().forEach(lang => {
+      this.http.get(`assets/markdown/about-${lang}.md`, { responseType: "text" }).subscribe(data => {
+        this.markdowns[lang] = data;
+      })
+    });
   }
 }
