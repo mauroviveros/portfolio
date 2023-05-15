65eaca82a4db135328ab1498ff087974201f2374
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-14 21:33:44 -0300

    feat(core.shared): :arrow_up: update angular/material dependencies

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 
 import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
+import { MatIconModule, MatIconRegistry } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { DomSanitizer } from "@angular/platform-browser";
 
 
 @NgModule({
@@ -11,7 +13,14 @@ import { MatToolbarModule } from "@angular/material/toolbar";
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatListModule,
     MatToolbarModule
   ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+  constructor(matIconRegistry: MatIconRegistry, domSanitizer: DomSanitizer){
+    matIconRegistry.addSvgIcon("instagram", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/instagram.svg"));
+    matIconRegistry.addSvgIcon("linkedin", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/linkedin.svg"));
+    matIconRegistry.addSvgIcon("github", domSanitizer.bypassSecurityTrustResourceUrl("assets/icons/socials/github.svg"));
+  }
+}
