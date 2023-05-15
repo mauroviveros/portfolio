caea9b4f1c001b8704eff6803e6d34a953b06048
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-14 21:36:22 -0300

    feat(core): :sparkles: add navbar component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit, inject } from "@angular/core";
+import { NavigationEnd, Router } from "@angular/router";
+import { Subscription, filter } from "rxjs";
 
 import { USER } from "src/environments/environment";
 
@@ -7,8 +9,22 @@ import { USER } from "src/environments/environment";
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  private subscription?: Subscription;
+  private router = inject(Router);
   profileURL  = USER.profileURL;
   fullname    = `${USER.name.join(" ")} ${USER.surname.join(" ")}`;
   role        = USER.role;
+  showNav     = false;
+
+
+  ngOnInit(){
+    this.subscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => { this.showNav = false; });
+  }
+
+  ngOnDestroy(){
+    this.subscription?.unsubscribe();
+  }
 }
