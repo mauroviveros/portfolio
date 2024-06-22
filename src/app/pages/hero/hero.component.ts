5f6c15bfe933e9090d3731377aa2c2da96f69ceb
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-06-21 22:23:30 -0300

    feat: :sparkles: add navbarComponent and init HeroComponent

---
+++ src/app/pages/hero/hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+
+}
---
+++ src/app/pages/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { NavigationComponent } from '../../components/navigation/navigation.component';
+import { SocialLinksComponent } from '../../components/social-links/social-links.component';
+
+@Component({
+  selector: 'portfolio-navbar',
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, NavigationComponent, SocialLinksComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
