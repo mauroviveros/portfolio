ec371709afe948726eed8e72ba05564da999b179
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-06-21 22:22:25 -0300

    feat: :sparkles: add navigation component

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'portfolio-navigation',
+  standalone: true,
+  imports: [MatIconModule, MatListModule, RouterLink, RouterLinkActive],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+  private readonly router = inject(Router);
+
+  readonly options = { exact: true };
+  readonly menu = this.router.config.map(route => {
+    return Object.assign(
+      { path: route.path },
+      route.data?.['navigation']
+    )
+  });
+}
