0c58df673333bd4f938a223444b01f5996ff227e
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-06-21 22:22:08 -0300

    feat: :sparkles: add footer and header components

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { UpperCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-footer',
+  standalone: true,
+  imports: [UpperCasePipe],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  readonly copyright = {
+    year: 2022,
+    owner: "Mauro Daniel Viveros",
+    site: "https://github.com/mauroviveros"
+  }
+}
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CV } from '../../../environments/cv';
+import { TitleCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'portfolio-header',
+  standalone: true,
+  imports: [TitleCasePipe],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  readonly name = CV.basics.name;
+  readonly picture = CV.basics.picture;
+  readonly label = CV.basics.label;
+}
