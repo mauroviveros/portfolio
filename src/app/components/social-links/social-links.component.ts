1d26f0a8d9684286536107e937e7547a77f906f9
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-07-17 22:17:05 -0300

    feat: :sparkles: upgrade dynamically iconRegistry

--- src/app/components/social-links/social-links.component.ts
+++ src/app/components/social-links/social-links.component.ts
@@ -3,26 +3,33 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { CV } from '../../../environments/cv';
-import { LowerCasePipe, TitleCasePipe } from '@angular/common';
+import { TitleCasePipe } from '@angular/common';
 import * as icons from 'simple-icons';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'portfolio-social-links',
   standalone: true,
-  imports: [LowerCasePipe, MatButtonModule, MatIconModule, MatTooltipModule],
+  imports: [MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './social-links.component.html',
-  styleUrl: './social-links.component.scss'
+  styleUrl: './social-links.component.scss',
+  providers: [TitleCasePipe]
 })
 export class SocialLinksComponent {
   private readonly iconRegistry = inject(MatIconRegistry);
   private readonly sanitizer = inject(DomSanitizer);
-
+  private readonly titleCase = inject(TitleCasePipe);
   readonly socials = CV.basics.profiles;
 
   constructor(){
-    this.iconRegistry.addSvgIconLiteral("instagram", this.sanitizer.bypassSecurityTrustHtml(icons.siInstagram.svg));
-    this.iconRegistry.addSvgIconLiteral("linkedin", this.sanitizer.bypassSecurityTrustHtml(icons.siLinkedin.svg));
-    this.iconRegistry.addSvgIconLiteral("github", this.sanitizer.bypassSecurityTrustHtml(icons.siGithub.svg));
+    this.socials.forEach(social => {
+      const iconName = `si${this.titleCase.transform(social.network)}` as keyof typeof icons;
+      const icon : icons.SimpleIcon | undefined = icons[iconName];
+      if(!icon) return;
+      this.iconRegistry.addSvgIconLiteral(
+        social.network,
+        this.sanitizer.bypassSecurityTrustHtml(icon.svg)
+      );
+    });
   }
 }
