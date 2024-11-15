bffdd8e3557f5d0be4147b5c803d483af32582e3
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-11-15 20:54:20 -0300

    fix: :iphone: add responsive in navbar

---
+++ src/app/components/navbar/components/copyright/copyright.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-copyright',
+  standalone: true,
+  imports: [],
+  templateUrl: './copyright.component.html',
+  styleUrl: './copyright.component.scss',
+})
+export class CopyrightComponent {}
