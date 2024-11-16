93d9da43af8fc1780ff986e5a6588054cbf86b6f
Mauro Daniel viveros <maurod.viveros@gmail.com>
2024-11-15 22:49:30 -0300

    fix: :iphone: fix responsive in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CopyrightComponent } from './components/copyright/copyright.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SocialLinksComponent } from './components/social-links/social-links.component';
+
+const MATERIAL_MODULES = [MatToolbarModule, MatIconModule, MatButtonModule];
 
 @Component({
   selector: 'portfolio-navbar',
   standalone: true,
   imports: [
+    MATERIAL_MODULES,
+    CopyrightComponent,
     HeaderComponent,
-    FooterComponent,
     NavigationComponent,
     SocialLinksComponent,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  active = false;
+}
