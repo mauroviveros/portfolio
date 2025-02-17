17d132911c211e0834bb722f4bc70bff6700ed89
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-17 17:18:42 -0300

    feat: :sparkles: update brand

---
+++ src/consts/site.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+    TITLE: "mviveros",
+    AUTHOR: "Mauro Daniel Viveros",
+    ROLE: "DEV WEB | DEV OPS",
+    DESCRIPTION: "site.description",
+    KEYWORDS: "Mauro Daniel Viveros, Developer, Fullstack, Web, Mobile, DevOps, Programming, Technologies, Software, Development, Mauro Viveros"
+};
