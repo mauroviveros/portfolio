c603e6f2fc10c285935b9973b0e7c6138bba6199
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-18 02:15:15 -0300

    feat: :sparkles: add links on Hero

---
+++ src/consts/links.ts
@@ -0,0 +1,5 @@
+export const LINKS = {
+    GITHUB: 'https://github.com/mauroviveros/',
+    LINKEDIN: 'https://www.linkedin.com/in/mauroviveros/',
+    CV: 'https://drive.google.com/file/d/1VcraKv9SzyZbt4AdjD0nrj4b5g8gS8LI/view?usp=sharing'
+}
