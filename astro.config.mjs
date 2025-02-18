1ddc9cd1e89e1b421c4a99632c0efec02bab9e42
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-17 21:20:30 -0300

    ci: :rocket: integrate Vercel deployment with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel()
 });
