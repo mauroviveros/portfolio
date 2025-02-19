8f66cb9beb5da2ee09b9e0ea7a1b3ec97e8fc3db
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-18 23:22:57 -0300

    fix: :bento: optimize assets

--- src/consts/site.ts
+++ src/consts/site.ts
@@ -4,5 +4,5 @@ export const SITE = {
     ROLE: "Senior DEV WEB | Semi Senior DEV OPS",
     DESCRIPTION: "Portfolio personal de Mauro Viveros. Desarrollo frontend, optimización web y automatización con DevOps.",
     KEYWORDS: "Mauro Daniel Viveros, Senior Frontend Engineer, DevOps, Angular, TypeScript, React, Web Performance, Remote, Freelance, Software Engineer, Frontend Developer, UI Developer, Web Developer, JavaScript, Cloud, CI/CD, GitHub Actions, AWS, Docker, Kubernetes",
-    FAVICON: "https://res.cloudinary.com/mviveros/image/upload/v1738690096/golang.png",
+    FAVICON: "https://res.cloudinary.com/mviveros/image/upload/v1739931515/golang.webp",
 };
