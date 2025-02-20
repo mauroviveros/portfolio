c463294ef4658580db2480d160ebb7b35d196e6a
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-20 16:55:48 -0300

    chore: :package: upgrade Tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  adapter: vercel()
+  adapter: vercel(),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
