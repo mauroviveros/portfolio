28c76c8a85f907a0b0527292cf24335486940751
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-21 00:53:18 -0300

    feat: :technologist: improve add astro-icon for Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
-
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mauroviveros.com.ar",
   adapter: vercel(),
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
 });
