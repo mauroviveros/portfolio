068c543bfd20e6011732024a6443355136576380
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-02-21 00:58:06 -0300

    fix: :art: improve new structure socialLinks

--- src/consts/socials.ts
+++ src/consts/socials.ts
@@ -1,19 +1,19 @@
 export const SOCIALS = [
     {
-      name: "Github",
-      icon: "github",
+      label: "Github",
+      icon: "line-md:github",
       href: "https://github.com/mauroviveros/",
       color: "#333333",
     },
     {
-      name: "LinkedIn",
-      icon: "linkedin",
+      label: "LinkedIn",
+      icon: "line-md:linkedin",
       href: "https://www.linkedin.com/in/mauroviveros/",
       color: "#0077B5",
     },
     {
-      name: "Instagram",
-      icon: "instagram",
+      label: "Instagram",
+      icon: "line-md:instagram",
       href: "https://instagram.com/maurogontier/",
       color: "#E4405F",
     },
