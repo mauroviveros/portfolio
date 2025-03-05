6338ee3250a53ac71be857c6b02fbfe0464540de
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-03-04 23:54:24 -0300

    feat: :globe_with_meridians: use translations with variables

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,8 +3,15 @@ import es from './es.json';
 const translations = { es, en: {} };
 export const locales = Object.keys(translations);
 
+const replaceVars = (string: string, variables?: Record<string, string>) => {
+    if(!variables) return string;
+    return string.replace(/\{\{(\w+)\}\}/g, (pattern, key) => {
+        return variables[key] ?? pattern;
+    });
+}
+
 export const useTranslations = (language?: string) => {
     if(!language) return (id: string) => id;
     const json = translations[language as  keyof typeof translations] as Record<string, string>;
-    return (id: string) => json[id] ?? id;
+    return (id: string, variables?: Record<string, string>) => replaceVars(json[id] ?? id, variables);
 }
